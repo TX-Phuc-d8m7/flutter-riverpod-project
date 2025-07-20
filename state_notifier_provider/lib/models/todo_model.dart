b61cd9f553db391179711a7b5af85e9ab31e2303
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-07-20 23:38:21 +0700

    Init StateNotifier Project

---
+++ state_notifier_provider/lib/models/todo_model.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+import 'package:uuid/uuid.dart';
+
+part 'todo_model.freezed.dart';
+
+Uuid uuid = const Uuid();
+
+@freezed
+class Todo with _$Todo {
+  const factory Todo({
+    required String id,
+    required String title,
+    required String description,
+    @Default(false) bool isCompleted,
+  }) = _Todo;
+}
