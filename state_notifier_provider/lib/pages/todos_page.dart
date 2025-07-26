da3fa6b88f97c8c2f8053a174e9cfa0e86d92a7e
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-07-26 23:09:16 +0700

    Daily Commit

--- state_notifier_provider/lib/pages/todos_page.dart
+++ state_notifier_provider/lib/pages/todos_page.dart
@@ -19,18 +19,6 @@ class TodosPage extends ConsumerWidget {
                 },
                 child: const Text('Back'),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/todos/1');
-                },
-                child: const Text('Go to Todo 1'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/todos/1');
-                },
-                child: const Text('Go to Todo 2'),
-              ),
             ],
           ),
         ],
